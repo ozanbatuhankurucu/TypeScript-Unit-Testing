dd8b5f8c665f99faaaddf2627c2427ad86e75c88
ozanbatuhankurucu <50013838+ozanbatuhankurucu@users.noreply.github.com>
2022-03-27 22:53:55 +0300

    second commit

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,6 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.add = void 0;
 function add(numbers) {
     let integers = numbers.split(',').map((x) => parseInt(x));
     let negatives = integers.filter((x) => x < 0);
@@ -6,5 +8,4 @@ function add(numbers) {
         throw new RangeError('Negatives are not allowed: ' + negatives.join(', '));
     return integers.filter((x) => x <= 1000).reduce((a, b) => a + b, 0);
 }
-let result = add('1, 2, 4, 5,-2,-5');
-console.log(result);
+exports.add = add;
---
+++ dist/tests/index.test.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const index_1 = require("../index");
+describe('testing index file', () => {
+    test('empty string should result in zero', () => {
+        expect((0, index_1.add)('')).toBe(0);
+    });
+    test('passing a single number should result in the number itself', () => {
+        expect((0, index_1.add)('1')).toBe(1);
+    });
+    test('code throws the expected exception if you pass negative numbers', () => {
+        expect(() => (0, index_1.add)('-1,-2,-3,5,7,2')).toThrow(RangeError);
+    });
+    test('passing a string with several numbers and verify whether the result is expected', () => {
+        expect((0, index_1.add)('1,2,3,4,5,6')).toBe(21);
+    });
+});
